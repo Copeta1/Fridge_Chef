7af45f6fb254bc268235f6d5c0bee295b28873de
Copeta1 <pecolajrobert1@gmail.com>
2024-09-06 16:27:32 +0200

    Mainpage Created

--- src/component/Mainpage/Mainpage.jsx
+++ src/component/Mainpage/Mainpage.jsx
@@ -1,9 +1,32 @@
 import Navbar from "../Navbar/Navbar";
+import "./mainpage.css";
+import Slika from "../../assets/images/slika.jpg";
 
 export default function Mainpage() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div className="mainpage-picture">
+        <img src={Slika} alt="slika" className="slika" />
+        <div className="text-overlay">
+          <h2>What is in your frigde?</h2>
+        </div>
+      </div>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          viverra nulla ante, et egestas dui pharetra eget. Ut eu ipsum mollis,
+          dignissim enim in, laoreet velit. Duis id iaculis felis. Nunc pulvinar
+          massa vestibulum, posuere nisl a, lobortis diam. Sed dignissim
+          consequat sem. Proin eleifend, mi et suscipit aliquet, ligula risus
+          fringilla justo, pretium hendrerit nunc erat vitae nulla. Nulla mauris
+          felis, tempus eget risus id, fringilla maximus dui. Donec sit amet
+          tempor diam, sit amet vehicula neque. Ut lobortis, felis non euismod
+          varius, enim quam mattis ante, sed egestas justo dui eu enim. Donec
+          sollicitudin ut massa a sagittis. Proin et pulvinar diam. Vivamus id
+          augue dui. In quis consectetur sem
+        </p>
+      </div>
+    </>
   );
 }
