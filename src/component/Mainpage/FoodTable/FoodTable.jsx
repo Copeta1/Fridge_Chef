09d033543469c74b09caf8e7c9f9c4f8c5ea95eb
Copeta1 <pecolajrobert1@gmail.com>
2024-09-10 12:41:33 +0200

    foodTable

--- src/component/Mainpage/FoodTable/FoodTable.jsx
+++ src/component/Mainpage/FoodTable/FoodTable.jsx
@@ -2,9 +2,25 @@ import "./foodTable.css";
 import { createData, rows, headCells } from "./data";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { Box, Paper, Table, TableContainer } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  IconButton,
+} from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { useState } from "react";
 
 export default function FoodTable() {
+  const [page, setPage] = useState(0);
+  const rowsperpage = 5;
+
   return (
     <>
       <div className="Top">
@@ -21,12 +37,43 @@ export default function FoodTable() {
         <Box sx={{ width: "100%" }}>
           <Paper sx={{ width: "100%, mb: 2" }}>
             <TableContainer>
-              <Table
-                sx={{ minWidth: 750 }}
-                alia-labelledby="tableTitle"
-              ></Table>
+              <Table sx={{ minWidth: 750 }} alia-labelledby="tableTitle">
+                <TableHead>
+                  <TableRow>
+                    {headCells.map((headCells) => (
+                      <TableCell
+                        key={headCells.id}
+                        padding={headCells.disablePadding ? "" : "normal"}
+                      >
+                        {headCells.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.id}>
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="center">{row.calories}</TableCell>
+                      <TableCell align="center">{row.fat}</TableCell>
+                      <TableCell align="center">{row.carbs}</TableCell>
+                      <TableCell align="center">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </TableContainer>
           </Paper>
+          <Box>
+            <IconButton>
+              <ArrowBackIosIcon />
+            </IconButton>
+            <IconButton>
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </Box>
         </Box>
       </div>
     </>
